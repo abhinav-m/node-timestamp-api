bc1144f6030464d38810f01960357af658e46c4c
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-10 22:10:35 +0530

    Added CORS headers

--- server/server.js
+++ server/server.js
@@ -12,6 +12,21 @@ const server = http.createServer((req, res) => {
     debugger;
     const { statusCode, content, contentType } = page;
     const responseHeader = contentType ? { "Content-Type": contentType } : {};
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    // Request methods you wish to allow
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // res.setHeader("Access-Control-Allow-Credentials", true);
     const responseContent = content ? content : "";
     res.writeHead(statusCode, responseHeader);
     res.end(responseContent, "utf-8");
